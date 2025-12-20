6611373c5574e31fceb2dbf0bc744666eff9f407
Tj <tjmagpantay23@gmail.com>
2025-12-20 21:20:24 +0800

    update gwa, iyaq q nalang e2

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -8,7 +8,7 @@ export default function EducationPage() {
       school: "Batangas State University – TNEU Lipa",
       location: "Lipa, Batangas",
       degree: "Bachelor of Science in Information Technology",
-      gwa: "INC man daw kami sa capstone",
+      gwa: "1.26",
       period: "Expected Graduation: July 2026",
       achievement: "Dean's Lister (1st Year – Present)",
     },
