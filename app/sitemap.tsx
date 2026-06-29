1ae308ce9d5af4761195e066bc5d623cc6451e82
Tj <tjmagpantay23@gmail.com>
2026-06-29 15:15:24 +0800

    created sitemap

---
+++ app/sitemap.tsx
@@ -0,0 +1,29 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://tj-magpantay.vercel.app/";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+  ];
+}
